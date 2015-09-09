5147a1fc80e787dbfce34fce7ec1b4ac96d06082
Björn Kowalzik <bjoern@kowalzik.berlin>
2015-09-09 11:06:45 +0200

    Minimales unstuck

--- src/torcs/hamanet/HamaNetDriver.java
+++ src/torcs/hamanet/HamaNetDriver.java
@@ -1,7 +1,6 @@
 package torcs.hamanet;
 
 import java.util.Arrays;
-
 import torcs.scr.Driver;
 import torcs.scr.Action;
 import torcs.scr.SensorModel;
@@ -13,20 +12,15 @@ import torcs.scr.SensorModel;
  * to the edges
  */
 public class HamaNetDriver extends Driver {
-
-	final int STATE_IDLE = 0;
-	final int STATE_CURVE = 1;
-	final int STATE_MUD = 2;
 	
-	double lane_angle = Math.PI / 19;
+	double lane_angle = Math.PI / 18;
 	double m_width = 0.4;
-	double minSpeed = 50;
-	
-	int state = STATE_IDLE;
-	boolean right = false;
+	double minSpeed = 100;
+	int stuck = 0;
 
 	private int tickcounter = 0;
 	private int myGear = 1;
+	
 	public Action control(SensorModel sm) {
 
 		// adjust tick counter
@@ -34,32 +28,45 @@ public class HamaNetDriver extends Driver {
 
 		// create new action object to send our commands to the server
 		Action action = new Action();
-		//Gather Telemetry
-		double trackAngle = sm.getAngleToTrackAxis();
-		double[] te = sm.getTrackEdgeSensors();
-		double distanceLeft = te[0];
-		double distanceRight = te[18];
-		double distanceFront = te[9];
+		System.out.println(sm.getTrackEdgeSensors()[9]);
+		System.out.println(sm.getTrackPosition());
+		System.out.println(sm.getAngleToTrackAxis());
+		System.out.println(stuck);
+		System.out.println("----------------");
+
+		if (Math.abs(sm.getTrackPosition()) > 1 && Math.abs(sm.getAngleToTrackAxis()) > ((5.0/9.0)*Math.PI))
+		{
+			if (sm.getSpeed() > 10)
+			{
+				action.brake = 1;
+				return action;
+			}
+			else
+			{
+				action.gear = (int)Math.signum(sm.getAngleToTrackAxis()*sm.getTrackPosition());
+				action.steering = action.gear*targetAngle(sm,0);
+			}
+		}
 		
 		//Curve Management
 		int curve = curveAhead(sm);
-		if (sm.getSpeed() < minSpeed) action.accelerate = 50;
-		//if (action.steering > 0 && Math.signum(curve) != Math.signum(sm.getTrackPosition())) action.brake = 1;
-		//if (action.steering == 0 && action.brake == 0 || sm.getSpeed() < minSpeed) action.accelerate = 1;
 		action.steering = targetLane(sm, curve*0.9);
+		if (curve == 0 && sm.getTrackEdgeSensors()[9] >= 100 || sm.getSpeed() < minSpeed) action.accelerate = 1;
+		
+		//System.out.println(sm.getTrackEdgeSensors()[9]);
 
-		//use rpm for upgear, vel for downgear?
+		//gear
 		if (sm.getRPM() >= 8000)
 		{
 			myGear = sm.getGear() + 1;
-		} else if(sm.getRPM() <= 3500)
+		} else if(sm.getRPM() <= 2000)
 		{
 			myGear = sm.getGear() - 1;
 		}
 		if (myGear == 7) myGear = 6;
 		if (myGear == 0) myGear = 1;
-		action.gear = myGear;		
-
+		action.gear = myGear;
+		
 		return action;
 	}
 	private int curveAhead(SensorModel sm)
@@ -91,16 +98,21 @@ public class HamaNetDriver extends Driver {
 		int l = 0;
 		int u2 = angles.length - 1;
 		int l2 = 0;
+		
+		c = 0;
 
 		for (int i = 0; i<angles.length-1; i++)
 		{
+			if (angles[i] != angles[i+1]) c++;
 			if (angles[u]) u--;
 			if (!angles[l]) l++;
 			if (!angles[u2]) u2--;
 			if (angles[l2]) l2++;
 		}
-		System.out.println(u + " " + l+ " " + u2 +" " + l2);
-		//if ((l+1 == u) && (l2+1 == u2)) return 0;
+		//System.out.println(Arrays.toString(angles));
+		//System.out.println(u + " " + l+ " " + u2 +" " + l2);
+		
+		if (c>4) return 0;
 		if (Math.min(u, l) >= 2) return -1;
 		if (Math.min(u2, l2) >= 2) return 1;
 		return 0;
