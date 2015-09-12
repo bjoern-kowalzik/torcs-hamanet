9207d23be8337d455b1bd84a5089e61abaa0a04b
Björn Kowalzik <bjoern@kowalzik.berlin>
2015-09-12 10:04:18 +0200

    Finale Abgabeversion

--- src/torcs/hamanet/HamaNetDriver.java
+++ src/torcs/hamanet/HamaNetDriver.java
@@ -15,8 +15,8 @@ import torcs.scr.SensorModel;
  */
 public class HamaNetDriver extends Driver {
 	
-	private double lane_angle = Math.PI / 18;
-	private double m_width = 0.4;
+	private double lane_angle = Math.PI / 9;
+	private double m_width = 0.7;
 	private double minSpeed = 100;
 	private int deadticks = 0;
 	private int myGear = 1;
@@ -41,7 +41,7 @@ public class HamaNetDriver extends Driver {
 		
 		//Curve Management
 		int curve = curveAhead(sm);
-		action.steering = targetLane(sm, curve*0.9);
+		action.steering = targetLane(sm, curve*0.7);
 		if (curve == 0 && sm.getTrackEdgeSensors()[9] >= 100 || sm.getSpeed() < minSpeed) action.accelerate = 1;
 		
 		//System.out.println(sm.getTrackEdgeSensors()[9]);
@@ -50,10 +50,10 @@ public class HamaNetDriver extends Driver {
 		speedAdaption(sm, action);
 		
 		//gear
-		if (sm.getRPM() >= 8000)
+		if (sm.getRPM() >= 8600)
 		{
 			myGear = sm.getGear() + 1;
-		} else if(sm.getRPM() <= 2000)
+		} else if(sm.getRPM() <= 3000)
 		{
 			myGear = sm.getGear() - 1;
 		}
@@ -62,22 +62,36 @@ public class HamaNetDriver extends Driver {
 		action.gear = myGear;
 		
 		//unstuck
-		if (Math.abs(sm.getTrackPosition())>1)
-		{
-			//action.steering = targetLane(sm,0);
-			action.brake = 0;
-			action.accelerate = 1;
-		}
-		
-		if (sm.getRPM()>100 && sm.getSpeed() < 5) deadticks++;
-		else deadticks = 0;
-		if (deadticks > 150)
-		{
-			action.gear = -1;
-			action.steering = Math.signum(action.gear*targetLane(sm, 0));
-			action.accelerate = 1;
-			deadticks = 0;
-		}
+        if (Math.abs(sm.getTrackPosition())>1)
+        {
+                //action.steering = targetLane(sm,0);
+                action.brake = 0;
+                action.accelerate = 1;
+        }
+       
+        if (sm.getRPM()>100 && sm.getSpeed() < 5) deadticks++;
+        else deadticks = 0;
+
+        if (deadticks > 150)
+        {
+                action.gear = (int)-Math.signum(sm.getGear());
+                action.steering = Math.signum(action.gear*targetLane(sm, 0));
+                action.accelerate = 0.3;
+                deadticks = 0;
+        }
+        else if (sm.getGear() == -1)
+        {
+                if ( Math.abs(sm.getTrackPosition()) > 0.5){
+                        action.gear = -1;
+                        action.steering = Math.signum(action.gear*targetLane(sm, 0));
+                } else if (Math.abs(sm.getSpeed()) > 0.1)
+                {
+                        action.accelerate = 0;
+                        action.brake = 1;
+                        action.steering = Math.signum(action.gear*targetLane(sm, 0));
+                }
+                else action.gear = 1;
+        }
 		
 		return action;
 	}
@@ -135,16 +149,15 @@ public class HamaNetDriver extends Driver {
 		double dircetionAng = getMax(sm.getTrackEdgeSensors());
 		double maxSpeed = getMaxSpeed(dircetionAng);
 		
-		//if(dircetionAng >= 199) maxSpeed = 250;
+		if(dircetionAng >= 90) maxSpeed = 300;
 		if(maxSpeed > sm.getSpeed()) {
 			action.accelerate = 1;
 			action.brake = 0;
-		}
+		}else
 		if(maxSpeed < sm.getSpeed()) {
 			if(sm.getSpeed() > 100 && dircetionAng > 150)
 				System.out.println("if1" + " direc" + dircetionAng +"macspeed" + maxSpeed);
 				action.accelerate = 0;
-				action.gear = myGear-1;
 				action.brake = 1;
 			if(sm.getSpeed() > 80 && dircetionAng < 80)
 				action.accelerate = 0;
@@ -153,17 +166,15 @@ public class HamaNetDriver extends Driver {
 			if(sm.getSpeed() > 60 && dircetionAng < 60)
 				action.accelerate = 0;
 				System.out.println("if4" + " direc" + dircetionAng +"macspeed" + maxSpeed);
-				action.brake = 0.7;
+				action.brake = 1;
 			if(sm.getSpeed() > 40 && dircetionAng < 40)
 				action.accelerate = 0;
 				System.out.println("if5" + " direc" + dircetionAng +"macspeed" + maxSpeed);
-				action.brake = 0.6;
+				action.brake = 1;
 			if(sm.getSpeed() > 20 && dircetionAng < 25)
 				action.accelerate = 0;
 				System.out.println("if6" + " direc" + dircetionAng +"macspeed" + maxSpeed);
-				action.brake = 0.5;
-				//action.gear = myGear-1;
-			
+				action.brake = 1;
 		}
 
 		System.out.println(maxSpeed + " " + dircetionAng);
@@ -171,7 +182,7 @@ public class HamaNetDriver extends Driver {
 	}
 	
 	private double getMaxSpeed(double focus){
-		return 3.6*(Math.sqrt(9.80665*1.3*focus));
+		return 3.6*(Math.sqrt(9.80665*1.4*focus));
 	}
 	
 	private double getMax(double [] array){
